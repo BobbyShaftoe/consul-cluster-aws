b92f3986ce9af8c2ff01f064ddeb9d83ad199559
nicksinclair <tech@halcyonnetworks.com.au>
2018-10-13 07:20:39 +0800

    Add elb configuration

--- terraform/consul-deployment/main.tf
+++ terraform/consul-deployment/main.tf
@@ -43,7 +43,39 @@ resource "aws_instance" "consul_instances" {
   user_data         = "${element(data.template_file.bootstrap.*.rendered, count.index)}"
 
   tags = "${merge(map("Name", format("%s-%s-%s", element(var.instance_names, 1+(count.index)%2), count.index, element(var.az_list, 1+(count.index+1)%3))), local.tag_map)}"
+}
 
 
+
+resource "aws_elb" "consul_cluster_elb" {
+  name = "consul-cluster-elb"
+
+  # The same availability zone as our instance
+  subnets = ["${module.subnet.consul_subnet_ids}"]
+
+  security_groups = ["${module.security_groups.consul_sec_gr_elb_id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  # The instance is registered automatically
+
+  instances                   = ["${aws_instance.consul_instances.*.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
 }
 
